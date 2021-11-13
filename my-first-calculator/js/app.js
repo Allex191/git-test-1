6de7c80331107329a0d6bb1bc37b9be063379a51
AP <alexeipetrea@yahoo.com>
2021-11-13 15:45:46 +0200

    started js for my first calc

--- my-first-calculator/js/app.js
+++ my-first-calculator/js/app.js
@@ -1,4 +1,18 @@
-/* const usrInput = Document.getElementsByClassName("calculator__input");
+const calculatorNumbers = document.querySelectorAll(
+  ".calculator__button--number"
+);
+const calculatorInput = document.querySelector(".calculator__input");
+const calculatorOperation = document.querySelectorAll(
+  ".calculator__button--operation"
+);
 
-usrInput.textContent = 12;
- */
+let calculatorValue = 0;
+
+const getUserNumbers = (number) => {
+  calculatorInput.value = number.textContent;
+  calculatorValue = calculatorInput.value;
+};
+
+for (const number of calculatorNumbers) {
+  number.addEventListener("click", getUserNumbers.bind(this, number));
+}
