e512395299267827c47b235efa8c47474addbe11
AP <alexeipetrea@yahoo.com>
2021-11-17 12:55:59 +0200

    deleted all my calculator logic except event listeners, trying again..

--- my-first-calculator/js/app.js
+++ my-first-calculator/js/app.js
@@ -12,54 +12,7 @@ const operationsLogic = {
   "÷": (x, y) => x / y,
   x: (x, y) => x * y,
 };
-
-let inferiorInputStorage = [];
-let inputNumbers = "";
-let memoryStock1 = null;
-let memoryStock2 = null;
-let result = 0;
-let currentOperation;
-
-const createResult = (equalSymbol) => {
-  result += operationsLogic[currentOperation](memoryStock1, memoryStock2);
-  mainInput.value = result;
-  inferiorInput.value = inferiorInputStorage.splice(0, 3, result);
-  inferiorInput.value = inferiorInputStorage.join("");
-};
-
-//receive userNumbers in string
-const addUserNumber = (number) => {
-  inputNumbers += number; //addstring of numbers from keyboard buttons
-  mainInput.value = inputNumbers; //show direct user input
-};
-
-//receive operation symbols in string
-const setMathOperation = (operation) => {
-  //1.read userNumber
-  //2.if currentOperation is null memoryStock = userNumber
-  //3.if currentOperation not empty
-  //1+1*
-  //2*
-  //2*5+
-
-  inferiorInputStorage.push(inputNumbers);
-  inferiorInputStorage.push(operation);
-  inferiorInput.value = inferiorInputStorage.join("");
-
-  if (memoryStock1 === null) {
-    memoryStock1 = +inputNumbers;
-    inputNumbers = "";
-    currentOperation = operation;
-  } else if (memoryStock2 === null && inputNumbers != "") {
-    memoryStock2 = +inputNumbers;
-    createResult();
-    memoryStock1 = null;
-    memoryStock2 = null;
-  }
-
-  inputNumbers = "";
-};
-
+/*  */
 //0-9 eventListener
 for (const number of numbersButtons) {
   number.addEventListener(
@@ -87,3 +40,58 @@ equalButton.addEventListener(
 // daca apas + a doua oara cifra se pune in memory dar in input arata rezultatul
 // daca apas egal in input arata rezultatul 5 dar in inferior arata 2+3=
 // dupa cind apas plus in inferior arata rezultatul  5+
+
+  //1.read userNumber
+  //2.if currentOperation is null memoryStock = userNumber
+  //3.if currentOperation not empty
+  //1+1*
+  //2*
+  //2*5+
+
+  
+// let inferiorInputStorage = [];
+// let inputNumbers = "";
+// let memoryStock1 = null;
+// let memoryStock2 = null;
+// let result = 0;
+// let currentOperation;
+
+// const createResult = (equalSymbol) => {
+//   result += operationsLogic[currentOperation](memoryStock1, memoryStock2);
+//   mainInput.value = result;
+//   inferiorInput.value = inferiorInputStorage.splice(0, 3, result);
+//   inferiorInput.value = inferiorInputStorage.join("");
+// };
+
+// //receive userNumbers in string
+// const addUserNumber = (number) => {
+//   inputNumbers += number; //addstring of numbers from keyboard buttons
+//   mainInput.value = inputNumbers; //show direct user input
+// };
+
+// //receive operation symbols in string
+// const setMathOperation = (operation) => {
+//   //1.read userNumber
+//   //2.if currentOperation is null memoryStock = userNumber
+//   //3.if currentOperation not empty
+//   //1+1*
+//   //2*
+//   //2*5+
+
+//   inferiorInputStorage.push(inputNumbers);
+//   inferiorInputStorage.push(operation);
+//   inferiorInput.value = inferiorInputStorage.join("");
+
+//   if (memoryStock1 === null) {
+//     memoryStock1 = +inputNumbers;
+//     inputNumbers = "";
+//     currentOperation = operation;
+//   } else if (memoryStock2 === null && inputNumbers != "") {
+//     memoryStock2 = +inputNumbers;
+//     createResult();
+//     memoryStock1 = null;
+//     memoryStock2 = null;
+//   }
+
+//   inputNumbers = "";
+// };
